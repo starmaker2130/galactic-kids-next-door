a03b05521d119be7f3e8f25c05f29ea4cde2d9b1
Patrice-Morgan de Goma <ongoly.patricemorgan@gmail.com>
2023-05-23 07:21:32 -0400

    adding new pARk pages

--- public/js/Lyoko.LoginToMain.js
+++ public/js/Lyoko.LoginToMain.js
@@ -2,27 +2,25 @@
 *
 * SECURE LOGIN TO THE PARK
 * ACCESS LYOKO FX AND IDE
-* v. 0.1.0.0
+* v. 0.0.9.0
 * by Patrice-Morgan de Goma
 *
 */
 var storage, database, AlbumAuthor, AlbumAuthorNoSpaces, AlbumName, AlbumNameNoSpaces, PromisedPause, AlbumTrackList;
-
 const config = {
-    apiKey: "AIzaSyDb25cWx6pMmh2xt54c_G5hS_rTDzpnpKw",
-    authDomain: "galactic-kids-next-door.firebaseapp.com",
-    databaseURL: "https://galactic-kids-next-door-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "galactic-kids-next-door",
-    storageBucket: "galactic-kids-next-door.appspot.com",
-    messagingSenderId: "273360155490",
-    appId: "1:273360155490:web:a752b1c28ad6156791f23b"
+  apiKey: "AIzaSyAzqXRxpmI9K7UMTsiuFU6b54SkSjNojkU",
+  authDomain: "lyoko-blockchain.firebaseapp.com",
+  databaseURL: "https://lyoko-blockchain-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lyoko-blockchain",
+  storageBucket: "lyoko-blockchain.appspot.com",
+  messagingSenderId: "820284895954",
+  appId: "1:820284895954:web:e69039cadfd002bc15b30b"
 };
-
 var windowIsLoaded = false;
 
 firebase.initializeApp(config);
 
-firebase.auth().signInWithEmailAndPassword("ceo@dronepong.com", "L3R0!d3G0M@")
+firebase.auth().signInWithEmailAndPassword("traderjoesdeliverydc@gmail.com", "L3R0!duL0@ng0")
   .then((userCredential) => {
     // Signed in
     console.log("signed in");
@@ -33,9 +31,6 @@ firebase.auth().signInWithEmailAndPassword("ceo@dronepong.com", "L3R0!d3G0M@")
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    console.log("couldn't sign in!")
-    console.log(errorCode);
-    console.log(errorMessage)
   });
 
 storage = firebase.storage();
